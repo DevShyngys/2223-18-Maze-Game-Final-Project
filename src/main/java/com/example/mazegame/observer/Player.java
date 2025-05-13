2d6be2b02cf33b5f69ae7adb5594343793a0a878
DevShyngys <chingizbulatov72@gmail.com>
2025-05-14 02:29:32 +0500

    Observer pattern is slightly changed

--- src/main/java/com/example/mazegame/observer/Player.java
+++ src/main/java/com/example/mazegame/observer/Player.java
@@ -2,6 +2,15 @@ package com.example.mazegame.observer;
 
 public class Player implements  Observer {
     @Override
+    public void onGameWon() {
+        System.out.println("YOU WIN!");
+    }
+
+    @Override
+    public void onTimeUp() {
+        System.out.println("Time up!");
+    }
+    @Override
     public void update() {
         System.out.println("Игрок переместился.");
     }
