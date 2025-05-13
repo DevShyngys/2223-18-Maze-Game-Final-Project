9a914fbeb13ae62e84e8076318bfea92b10f940e
DevShyngys <chingizbulatov72@gmail.com>
2025-05-14 02:23:58 +0500

    Strategy pattern is added for various map generation

--- src/main/java/com/example/mazegame/strategy/MovementStrategy.java
+++ src/main/java/com/example/mazegame/strategy/MovementStrategy.java
@@ -1,6 +1,6 @@
 package com.example.mazegame.strategy;
+import com.example.mazegame.MazeGame;
 
 public interface MovementStrategy {
-    void move();
-
+    void move(MazeGame game, int x, int y);
 }
