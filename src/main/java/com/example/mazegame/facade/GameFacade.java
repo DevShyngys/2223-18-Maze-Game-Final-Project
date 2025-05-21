cf69485979991f2516b9331df71f5799b6237497
Shyngys Bulatov <chingizbulatov72@gmail.com>
2025-05-21 17:21:08 +0500

    Update GameFacade.java

--- src/main/java/com/example/mazegame/facade/GameFacade.java
+++ src/main/java/com/example/mazegame/facade/GameFacade.java
@@ -1,17 +1,10 @@
 package com.example.mazegame.facade;
 
-import com.example.mazegame.strategy.DeterministicMovementStrategy;
 import com.example.mazegame.MazeGame;
-import com.example.mazegame.strategy.MovementStrategy;
 
 public class GameFacade {
-    private static MazeGame game;
-
-    public GameFacade(MazeGame game) {
-        this.game = game;
-    }
-
     public static void startGame(MazeGame game) {
-        game.start();
+        game.generateMaze();
+        game.createAndShowGUI();
     }
 }
